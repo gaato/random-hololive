1a39becd4566b7204e336bff1f57345208292dc1
gaato <g.furuya@gaato.net>
2023-08-02 17:58:57 +0900

    Mention in log

--- src/libs.py
+++ src/libs.py
@@ -1,13 +1,15 @@
 import requests
 
 
+USER_ID = 572432137035317249
+
 class DiscordStream:
     def __init__(self, url):
         self.url = url
 
     def write(self, text):
         if 'ERROR' in text:
-            content = f'```diff\n- {text}\n```'
+            content = f'<@!{USER_ID}>\n```diff\n- {text}\n```'
         elif 'WARNING' in text:
             content = f'```diff\n+ {text}\n```'
         else:
